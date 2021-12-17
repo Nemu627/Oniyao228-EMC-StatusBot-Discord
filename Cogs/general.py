f68e12632c3470f50f847fa13551bcd87dd2dd04
音夢?! <82650472+Nemu627@users.noreply.github.com>
2021-12-17 17:43:54 +0900

    add : 取得するやつの試作

--- Cogs/general.py
+++ Cogs/general.py
@@ -1,1 +1,22 @@
+import discord
+from discord.ext import commands
+import asyncio
+import aiohttp
 
+class AppCmdVariety(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        self._last_member = None
+    
+    @commands.command()
+    async def test(self, ctx):
+        async with aiohttp.ClientSession() as session:
+            async with session.get(
+                "http://earthmc-api.herokuapp.com/allplayers/Oniyao228"
+            ) as response:
+                res = await response.json()
+                embed = discord.Embed(title="test",description=res["nation"])
+                await ctx.reply(embed=embed)
+
+def setup(bot):
+    return bot.add_cog(AppCmdVariety(bot))
