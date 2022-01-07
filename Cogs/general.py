984bad0142612c95c4511d6379027ed701ac27f8
音夢?! <82650472+Nemu627@users.noreply.github.com>
2022-01-07 19:04:17 +0900

    clean : 整理

--- Cogs/general.py
+++ Cogs/general.py
@@ -12,13 +12,9 @@ class AppCmdVariety(commands.Cog):
         async def loop():
             resident = emc.Resident("Oniya0228", data=await get_data())
             if resident.online:
-                await self.bot.change_presence(status=discord.Status.online,
-                                               activity=discord.Activity(name=f"Oniyao228 is online at EarthMC",
-                                                                         type=3))
+                await self.bot.change_presence(status=discord.Status.online, activity=discord.Activity(name=f"Oniyao228 is online at EarthMC", type=3))
             else:
-                await self.bot.change_presence(status=discord.Status.idle,
-                                               activity=discord.Activity(name=f"Oniyao228 is offline at EarthMC",
-                                                                         type=3))
+                await self.bot.change_presence(status=discord.Status.idle, activity=discord.Activity(name=f"Oniyao228 is offline at EarthMC", type=3))
         loop.start()
 
 def setup(bot):
