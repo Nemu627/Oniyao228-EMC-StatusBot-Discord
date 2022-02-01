c2ea909e9e0d5af04138369c5ebcb72e6ab91980
音夢?! <82650472+Nemu627@users.noreply.github.com>
2022-02-01 19:38:13 +0900

    fix : 一回全部取得してからテキストチャンネルを判別

--- Cogs/global-chat.py
+++ Cogs/global-chat.py
@@ -8,32 +8,35 @@ class AppCmdGlobalChat(commands.Cog):
 
     @commands.Cog.listener()
     async def on_message(self,message):
-        global_channel_name = "oniyama-gc"
-        if message.channel.topic == global_channel_name:
-            if message.author.bot:
-                return
-            for channel in message.guild.text_channels:
-                if channel.topic == global_channel_name:
-                    if channel == message.channel:
-                        continue
-                    embed=discord.Embed(description=message.content, color=0x9B95C9)
-                    embed.set_author(name=f"{message.author}",icon_url=message.author.avatar.url)
-                    embed.set_footer(text=f"{message.guild.name}",icon_url=message.guild.icon.url)
-                    if message.attachments != []:
-                        embed.set_image(url=message.attachments[0].url)
-                    if message.reference:
-                        reference_msg = await message.channel.fetch_message(message.reference.message_id)
-                        if reference_msg.embeds and reference_msg.author == self.bot.user:
-                            reference_message_content = reference_msg.embeds[0].description
-                            reference_message_author = reference_msg.embeds[0].author.name
-                        elif reference_msg.author != self.bot.user:
-                            reference_message_content = reference_msg.content
-                            reference_message_author = reference_msg.author.name+'#'+reference_msg.author.discriminator
-                        reference_content = ""
-                        for string in reference_message_content.splitlines():
-                            reference_content += "> " + string + "\n"
-                        embed.add_field(name=reference_message_author.name, value=reference_content, inline=True)
-                    await channel.send(embed=embed)
-
+        if message.author.bot:
+            return
+        topic = "oniyama-gc"
+        if message.channel.topic == topic:
+            for channel in self.bot.get_all_channels():
+                if channel.category is None:
+                    continue
+                for channel in channel.category.text_channels:
+                    if channel.topic == topic:
+                        if channel == message.channel:
+                            continue
+                        embed=discord.Embed(description=message.content, color=0x9B95C9)
+                        embed.set_author(name=f"{message.author}",icon_url=message.author.avatar_url)
+                        embed.set_footer(text=f"{message.guild.name}",icon_url=message.guild.icon_url)
+                        if message.attachments != []:
+                            embed.set_image(url=message.attachments[0].url)
+                        if message.reference:
+                            reference_msg = await message.channel.fetch_message(message.reference.message_id)
+                            if reference_msg.embeds and reference_msg.author == self.bot.user:
+                                reference_message_content = reference_msg.embeds[0].description
+                                reference_message_author = reference_msg.embeds[0].author.name
+                            elif reference_msg.author != self.bot.user:
+                                reference_message_content = reference_msg.content
+                                reference_message_author = reference_msg.author.name+'#'+reference_msg.author.discriminator
+                            reference_content = ""
+                            for string in reference_message_content.splitlines():
+                                reference_content += "> " + string + "\n"
+                            embed.add_field(name=reference_message_author.name, value=reference_content, inline=True)
+                        await channel.send(embed=embed)
+    
 def setup(bot):
     return bot.add_cog(AppCmdGlobalChat(bot))
