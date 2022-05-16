921719946617d756db7b26e7eb7340bb335dfea3
音夢?! <82650472+Nemu627@users.noreply.github.com>
2022-05-17 06:39:24 +0900

    clean : 要らない部分消した

--- discordbot.py
+++ discordbot.py
@@ -2,25 +2,18 @@ import discord
 from discord.ext import commands
 import os
 
-intents = discord.Intents.all()
-client = discord.Client(intents=intents)
 bot = commands.Bot(
     command_prefix=["O!", "o!"],
     help_command=None,
-    intents=intents,
+    intents=discord.Intents.all(),
     allowed_mentions=discord.AllowedMentions(replied_user=False, everyone=False),
     case_insensitive=True
 )
 
 token = os.environ["token"]
 
-def restart_bot():
-  os.execv(sys.executable, ['python'] + sys.argv)
-
 bot.load_extension("jishaku")
 
-bot.load_extension("Cogs.event")
 bot.load_extension("Cogs.general")
-bot.load_extension("Cogs.global-chat")
 
 bot.run(token)
